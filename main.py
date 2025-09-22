d5cd029f26f7e9ff14349aa9c07fb1c3094a2178
sahilgk-cms <sahil@catalysts.org>
2025-09-22 14:26:14 +0530

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+import sys
+
+current_path = os.path.abspath(__file__)
+root_path = os.path.dirname(os.path.dirname(current_path))
+sys.path.append(root_path)
+
+#run the scrapers
+#subprocess.run(["python", os.path.join("pipeline", "scrapers", "google_alerts_scraper.py")])
+#subprocess.run(["python", os.path.join("pipeline", "scrapers", "newsapi_scraper.py")])
+subprocess.run(["python", os.path.join("pipeline", "scrapers", "serpapi_scraper.py")])
+
