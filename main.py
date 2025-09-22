8f37bb555399afd44b66e8eaf91906796c86c4c1
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-09-22 14:27:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ root_path = os.path.dirname(os.path.dirname(current_path))
 sys.path.append(root_path)
 
 #run the scrapers
-#subprocess.run(["python", os.path.join("pipeline", "scrapers", "google_alerts_scraper.py")])
-#subprocess.run(["python", os.path.join("pipeline", "scrapers", "newsapi_scraper.py")])
+subprocess.run(["python", os.path.join("pipeline", "scrapers", "google_alerts_scraper.py")])
+subprocess.run(["python", os.path.join("pipeline", "scrapers", "newsapi_scraper.py")])
 subprocess.run(["python", os.path.join("pipeline", "scrapers", "serpapi_scraper.py")])
 
